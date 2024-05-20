41756d8bc4fa631c430b5963f11aa2e5e5c6d2cc
Andrew Bell <andrewbell545@gmail.com>
2024-05-20 18:29:38 +0100

    moved api key and made game cards.

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,41 +2,43 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { AxiosError, CanceledError } from "axios";
 
-interface Game {
-    id: number;
-    name: string;
-  }
-  
-  interface FetchGamesResponse {
-    count: number;
-    results: Game[];
-  }
-
-const useGames = () => {
-    const [games, setGames] = useState<Game[]>([]);
-    const [error, setError] = useState("");
-  
-    useEffect(() => {
-      const controller = new AbortController();
-
-      const fetchGames = async () => {
-        try {
-          const response = await apiClient.get<FetchGamesResponse>("/games", {signal: controller.signal});
-  
-          setGames(response.data.results);
-        } catch (err) {
-            if(err instanceof CanceledError) return;
-          setError((err as AxiosError).message);
-        }
-
-        return () => controller.abort();
-      };
-  
-      fetchGames();
-    }, []);
-
-    return {games, error};
+export interface Game {
+  id: number;
+  name: string;
+  background_image: string;
 }
 
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    const fetchGames = async () => {
+      try {
+        const response = await apiClient.get<FetchGamesResponse>("/games", {
+          signal: controller.signal,
+        });
+
+        setGames(response.data.results);
+      } catch (err) {
+        if (err instanceof CanceledError) return;
+        setError((err as AxiosError).message);
+      }
+
+      return () => controller.abort();
+    };
+
+    fetchGames();
+  }, []);
+
+  return { games, error };
+};
 
 export default useGames;
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+const { VITE_RAWR_API_KEY } = import.meta.env;
+
 export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params: {
-        key: 'f1efaa71288941179105b24213a44778'
-    }
-})
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: VITE_RAWR_API_KEY,
+  },
+});
